ef0ea08d921cedcd704e0325b6e29eaf4250fd89
Joshua Davis <pgmjsd@gmail.com>
2023-01-09 16:09:42 -0500

    start working on link tables

---
+++ README.md
@@ -0,0 +1,37 @@
+# notion2obsidian
+
+`notion2obsidian` converts Notion exports into Obsidian markdown files/folders.
+
+# Features
+
+Details about the conversion that `notion2obsidian` does...
+
+## Rename Folders and Notes
+
+* **Removes the UUIDs** that Notion adds to folders and to markdown files.  Internal links between markdown files use the modified folder / file names.
+
+## Translate Links
+
+* Internal links need to reference the renamed files and folders.
+* Internal links also need to be in Obsidian Internal Link format.
+* External links stay the same.
+
+## Transform CSV Files Into Tables
+
+* Notion exports "database" pages as CSV files.   These are translated into markdown files with a markdown table in them.
+
+## Work in Progress
+
+* Translate `Tags: foo, bar, baz` into Obsidian tags.
+* Remove first heading if it's the same as the page name.
+* Detect "link tables" and link the names to the corresponding notes.
+
+# Installation
+
+... to be written
+
+
+# Usage
+
+... to be written
+
--- src/table.rs
+++ src/table.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::io::Write;
+use log::info;
 
 use crate::file_helper::{open_output_file};
 
@@ -7,14 +8,19 @@ pub fn convert_csv_to_markdown(old_path: &Path, new_path: &Path) -> anyhow::Resu
 
     let mut reader = csv::Reader::from_path(old_path)?;
     let mut writer = open_output_file(new_path)?;
+    let mut link_first_column = false;
 
     // First, write the headers.
     if reader.has_headers() {
         let headers = reader.headers()?;
         let mut header_lengths : Vec<usize> = Vec::new();
-        for header in headers {
+        for (idx, header) in headers.iter().enumerate() {
             write!(writer, "|{}", header)?;
             header_lengths.push(header.len());
+            // If the first column is "Name", try to transform the column values into links.
+            if idx == 0 && header == "Name" {
+                link_first_column = true;
+            }
         }
         writeln!(writer, "|")?;
         for header_length in header_lengths {
@@ -25,8 +31,13 @@ pub fn convert_csv_to_markdown(old_path: &Path, new_path: &Path) -> anyhow::Resu
 
     for row in reader.records() {
         let row = row?;
-        for field in row.iter() {
-            write!(writer, "|{}", field)?;
+        for (column, field) in row.iter().enumerate() {
+            if link_first_column &&  column == 0 {
+                info!("Trying to link {}", field);
+                write!(writer, "|{}", field)?;
+            } else {
+                write!(writer, "|{}", field)?;
+            }
         }
         writeln!(writer, "|")?;
     }
@@ -35,4 +46,12 @@ pub fn convert_csv_to_markdown(old_path: &Path, new_path: &Path) -> anyhow::Resu
     writeln!(writer, "#notion2obsidian #csvimport")?;
     writer.flush()?;
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_convert_csv_to_markdown() {
+        assert!(true);
+    }
 }
