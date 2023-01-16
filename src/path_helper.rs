0bc38471e7180c851fc12377570f3b5222238e5d
Joshua Davis <pgmjsd@gmail.com>
2023-01-15 22:59:09 -0500

    add 'component_to_string'

--- src/path_helper.rs
+++ src/path_helper.rs
@@ -26,6 +26,11 @@ pub fn path_to_string(path: &Path) -> Result<String> {
     Ok(String::from(path.to_str().ok_or(anyhow!("Invalid Path!"))?))
 }
 
+/// Convert a path component into a String Result.
+pub fn component_to_string(component: &std::path::Component) -> Result<String> {
+    osstr_to_string(Some(component.as_os_str()))
+}
+
 /// Gets the file extension as a String, no default value.
 pub fn get_extension(path: &Path) -> Result<String> {
     osstr_to_string(path.extension())
@@ -47,8 +52,34 @@ mod tests {
     }
 
     #[test]
+    fn test_osstr_to_string() {
+        let osstr = OsStr::new("foo");
+        let s = osstr_to_string(Some(osstr)).unwrap();
+        assert_eq!(s, "foo");
+    }
+
+    #[test]
+    fn test_component_to_str() {
+        let path = Path::new("/tmp/foo/bar");
+        let strs = path.components().map(|c| {
+            component_to_string(&c).unwrap()
+            }).collect::<Vec<String>>();
+        assert_eq!(strs.len(), 4);
+        assert_eq!(strs[1], "tmp");
+        assert_eq!(strs[2], "foo");
+        assert_eq!(strs[3], "bar");
+    }
+
+    #[test]
     fn test_get_extension() {
         let path = Path::new("test.tXt");
         assert_eq!(get_extension(&path).unwrap().to_lowercase(), "txt");
     }
+
+    #[test]
+    fn test_is_zip_file() {
+        let path = Path::new("test.zip");
+        assert!(is_zip_file(&path).unwrap());
+    }
+
 }
